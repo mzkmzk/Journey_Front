d5c65def326406bad42feb9e4ff75339a14f30ed
Mai ZhiKun <maizk@haplox.com>
2016-07-18 15:08:22 +0800

    add

--- Src/Components/Panels/Panels.js
+++ Src/Components/Panels/Panels.js
@@ -10,8 +10,10 @@ export default class Panels extends Component {
 
     login_webibo(){
         WB2.login(function() {
-            //callback function
+            alert(WB2.checkLogin());
+            alert('执行完毕');
         })
+        alert(WB2.checkLogin());
     }
 
     render() {
@@ -22,7 +24,7 @@ export default class Panels extends Component {
                     <h3>登录1</h3>
                     <section>
                         <section>
-                            <i className="fa fa-weibo " aria-hidden="true" onClick="" ></i>
+                            <i className="fa fa-weibo " aria-hidden="true" onClick={() => this.login_weibo()} ></i>
                         </section>
                     </section>
                 </section>
