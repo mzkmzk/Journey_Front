adb0556455c9b03ded89c01dff419e864dc3895f
Mai ZhiKun <maizk@haplox.com>
2016-10-09 12:56:09 +0800

    fix env

--- Src/Global/JS/Environment.js
+++ Src/Global/JS/Environment.js
@@ -1,12 +1,11 @@
 export default class Environment {
     constructor(){
         const href = window.location.href
+        this.innerURL = 'inner.journey.404mzk.com'
         if (href.indexOf('test.journey.404mzk.com') !== -1){
             this.environment = 'test'
             this.sinaAppKey = '1911849944'
-            this.innerURL = 'inner.journey.404mzk.com'
-        }
-        else {
+        }else {
             this.environment = 'production'
             this.sinaAppKey = '2854977325'
         }
