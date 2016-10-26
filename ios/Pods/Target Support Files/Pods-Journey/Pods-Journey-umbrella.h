0cb9f1dbbaf0966407bd838fa165c26e983c149c
Mai ZhiKun <maizk@haplox.com>
2016-10-26 18:00:05 +0800

    commit

---
+++ ios/Pods/Target Support Files/Pods-Journey/Pods-Journey-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_JourneyVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_JourneyVersionString[];
+
---
+++ ios/Pods/Target Support Files/Pods-JourneyUITests/Pods-JourneyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JourneyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_JourneyUITests
+@end
