dfb0e92aa98e84d584f2fc499581bf04535f2a22
Mai ZhiKun <maizk@haplox.com>
2016-11-02 18:00:07 +0800

    commit

--- index.ios.js
+++ index.ios.js
@@ -20,7 +20,7 @@ export default class Journey extends Component {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          Hello,I am K
         </Text>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
