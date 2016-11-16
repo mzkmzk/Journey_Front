f28233ab69ae7a1494554b82293e2faf668c6d5b
麦志坤 <maizk@haplox.com>
2016-11-16 18:00:06 +0800

    commit

--- index.ios.js
+++ index.ios.js
@@ -9,88 +9,42 @@
 var React = require('react');
 var ReactNative = require('react-native');
 var {
+  AppRegistry,
   Image,
   Platform,
   StyleSheet,
   Text,
   TouchableHighlight,
   TouchableNativeFeedback,
-  View
+  View,
+  ScrollView,
+  UIScrollView,
+  ImageView
 } = ReactNative;
 
-var getStyleFromScore = require('./getStyleFromScore');
-var getImageSource = require('./getImageSource');
-var getTextFromScore = require('./getTextFromScore');
+
 
 class Journey extends React.Component {
   render() {
-    var criticsScore = this.props.movie.ratings.critics_score;
-    var TouchableElement = TouchableHighlight;
-    if (Platform.OS === 'android') {
-      TouchableElement = TouchableNativeFeedback;
-    }
+
     return (
-      <View>
-        <TouchableElement
-          onPress={this.props.onSelect}
-          onShowUnderlay={this.props.onHighlight}
-          onHideUnderlay={this.props.onUnhighlight}>
-          <View style={styles.row}>
-            <Image
-              source={getImageSource(this.props.movie, 'det')}
-              style={styles.cellImage}
-            />
-            <View style={styles.textContainer}>
-              <Text style={styles.movieTitle} numberOfLines={2}>
-                {this.props.movie.title}
-              </Text>
-              <Text style={styles.movieYear} numberOfLines={1}>
-                {this.props.movie.year}
-                {' '}&bull;{' '}
-                <Text style={getStyleFromScore(criticsScore)}>
-                  Critics {getTextFromScore(criticsScore)}
-                </Text>
-              </Text>
-            </View>
-          </View>
-        </TouchableElement>
-      </View>
-    );
+      <UIScrollView>
+        <ImageView source={{uri: 'http://my.apache.com/K-Report/Example/loadtime/img/girl2.jpg'}} />
+        <Text>
+          On iOS, a React Native ScrollView uses a native UIScrollView.
+          On Android, it uses a native ScrollView.
+
+          On iOS, a React Native Image uses a native UIImageView.
+          On Android, it uses a native ImageView.
+
+          React Native wraps the fundamental native components, giving you
+          the performance of a native app, plus the clean design of React.
+        </Text>
+      </UIScrollView>
+      );
   }
 }
 
-var styles = StyleSheet.create({
-  textContainer: {
-    flex: 1,
-  },
-  movieTitle: {
-    flex: 1,
-    fontSize: 16,
-    fontWeight: '500',
-    marginBottom: 2,
-  },
-  movieYear: {
-    color: '#999999',
-    fontSize: 12,
-  },
-  row: {
-    alignItems: 'center',
-    backgroundColor: 'white',
-    flexDirection: 'row',
-    padding: 5,
-  },
-  cellImage: {
-    backgroundColor: '#dddddd',
-    height: 93,
-    marginRight: 10,
-    width: 60,
-  },
-  cellBorder: {
-    backgroundColor: 'rgba(0, 0, 0, 0.1)',
-    height: StyleSheet.hairlineWidth,
-    marginLeft: 4,
-  },
-});
 
 
 AppRegistry.registerComponent('Journey', () => Journey);
